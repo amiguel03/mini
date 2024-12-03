e9cabe8f3c8d19cd7599dfb08ef6dfe612635f32
amiguel03 <amiguel-@student.42madrid.com>
2024-12-03 13:08:45 +0100

    subo errorres de pipe y su funciones correspondientes que necesitare luego

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amiguel- <amiguel-@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/12 11:03:09 by amiguel-          #+#    #+#             */
-/*   Updated: 2024/11/28 12:24:49 by amiguel-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -21,6 +9,7 @@
 # include <fcntl.h>
 # include <dirent.h>
 # include <sys/wait.h>
+# include <stdbool.h>
 # include <limits.h>
 # include <errno.h>
 # include <signal.h>
---
+++ src/parser/check_errors.c
@@ -0,0 +1,72 @@
+#include "../../inc/minishell.h"
+
+int	ft_locate_next_quote(int i, char *input, char quote)
+{
+	while (input && input[i] && input[i] != quote)
+		i++;
+	return (i);
+}
+
+int	ft_nothing_r(char *input, int i)
+{
+	while (i >= 0 && input[i] && input[i] == ' ')
+		i--;
+	if (i == -1)
+		return (1);
+	return (0);
+}
+
+int	ft_nothing(char *input, int i)
+{
+	while (input[i] && input[i] == ' ')
+		i++;
+	if (input[i] == '\0')
+		return (1);
+	return (0);
+}
+
+int valid_pipe(char *input, int i)
+{
+	bool	character;
+
+	if (i - 1 == -1)
+		return (-1);
+	i--;
+	while(input[i])
+	{
+		if (input[i] == '|' && character == false)
+		{
+			printf("Error pipe\n");
+			return (-1);
+		}
+		if (input[i] == '|' && character == true)
+			return (0);
+		if (input[i] != '|' && input[i] != ' ')
+			character = true;
+		i--;
+	}
+	return (0);
+}
+
+int	error_pipe(char *input)
+{
+	int i;
+
+	i = 0;
+	while (input[i] != '\0')
+	{
+		printf("input[%d]: %c\n", i, input[i]);
+		if (input[i] == '"' || input[i] == '\'')
+		{
+			i = ft_locate_next_quote(i + 1, input, input[i]);
+			if (input[i] == '\0')
+				return (-1);
+		}
+		if (input[i] == '|' && (ft_nothing_r(input, i - 1) == 1 || ft_nothing(input, i + 1) == 1))
+			return (-1);
+		if (input[i] == '|' && valid_pipe(input, i) == -1)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
